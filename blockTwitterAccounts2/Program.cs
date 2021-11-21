c577d4ac7656790bc1b1936f995b4fc908e9b8e8
aoaoaoaoaoaoaoi <40077710+aoaoaoaoaoaoaoi@users.noreply.github.com>
2021-11-21 17:52:48 +0900

    add program

---
+++ blockTwitterAccounts2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using Tweetinvi;
+using System.IO;
+using System.Text;
+
+namespace BlockTwitterAccouts
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("**********************");
+            Console.WriteLine("Hi!! Success!!!!!!");
+            Console.WriteLine("**********************");
+        }
+    }
+}
