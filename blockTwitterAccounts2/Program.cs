691aa3369adf89d5a315ad13aeb475a7b5fb4a02
aoaoaoaoaoaoaoi <40077710+aoaoaoaoaoaoaoi@users.noreply.github.com>
2021-11-21 21:49:44 +0900

    fix a csv file's name

--- blockTwitterAccounts2/Program.cs
+++ blockTwitterAccounts2/Program.cs
@@ -11,7 +11,7 @@ namespace BlockTwitterAccouts
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
             var file = File.ReadAllText("./ngWordText/test.csv", Encoding.UTF8);
             var ngWordList = file.Split(',');
